2b97dff78996bb40434d612f9ed32ffa75fe4df0
antoniocarvajal938 <carvajalwk@gmail.com>
2026-01-25 13:18:07 +0100

    Complete test documentation and finalize project

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,25 +1,7 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
+// Custom Cypress commands can be defined here if needed.
 //
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
+// This project follows a Page Object Model (POM) approach,
+// therefore most UI interactions are encapsulated within
+// page classes instead of custom commands.
 //
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// This file is intentionally kept minimal.
